9366cdec6d540aa2c3dabe7553cc9607946242e4
213026 <matea.blazheska@students.finki.ukim.mk>
2024-09-20 22:26:24 +0200

    Initial commit

---
+++ src/main/resources/db/migration/V1_3__initial3.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.team (id, name) VALUES
+                                       (1, 'Lakers'),
+                                       (2, 'Warriors'),
+                                       (3, 'Celtics');
---
+++ src/main/resources/db/migration/V1_4__initial4.sql
@@ -0,0 +1,4 @@
+INSERT INTO player (id, name, bio, points_per_game, position, team_id, votes) VALUES
+                                                                                  (1, 'LeBron James', 'One of the greatest basketball players', 27.2, 'F', 1, 120),
+                                                                                  (2, 'Stephen Curry', 'Best 3-point shooter in the NBA', 24.3, 'G', 2, 150),
+                                                                                  (3, 'Jayson Tatum', 'Rising star of the Celtics', 22.5, 'F', 3, 110);
